0652880bd2d10b898ba6bd35324336ae3e7272b2
gcodegroup <gcodegroup@gmail.com>
2018-10-24 11:47:45 +0100

    primefaces input month picker

---
+++ web-applications/primefaces-example/src/main/java/com/etlsolutions/examples/primefaces/event/InputEventValueChangeView.java
@@ -0,0 +1,27 @@
+package com.etlsolutions.examples.primefaces.event;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author zc
+ */
+
+@ManagedBean
+@ViewScoped
+public class InputEventValueChangeView {
+    
+    private static final long serialVersionUID = 958593203991107684L;
+    
+    private String startDateString = "12/2008";
+
+    public String getStartDateString() {
+        return startDateString;
+    }
+
+    public void setStartDateString(String startDateString) {
+        this.startDateString = startDateString;
+    }
+   
+}
